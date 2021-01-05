7cf323d8aac9d653c4ca01b730db053acc081394
Shrikant Kale <milind_d@gmail.com>
2021-01-05 14:08:36 +0530

    header added above the table

--- e-commerce/src/components/TableView.jsx
+++ e-commerce/src/components/TableView.jsx
@@ -1,111 +1,111 @@
 import React, { useEffect, useState } from "react";
-import { Table,Container } from "react-bootstrap";
-import Moment from 'moment';
+import { Table, Container, Row, Col } from "react-bootstrap";
+import Moment from "moment";
 function TableView() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
   const response = [
     {
-      "id": 1,
-      "facility": 0,
-      "region": "ALL",
-      "lookupName": "ReasonCode",
-      "description": "Reason Code Config",
-      "userId": "Suresh",
-      "createdDttm": "2020-08-25T18:32:39.237+00:00",
-      "modifiedDttm": "2020-08-25T18:32:39.237+00:00",
-      "lookupDetails": [
+      id: 1,
+      facility: 0,
+      region: "ALL",
+      lookupName: "ReasonCode",
+      description: "Reason Code Config",
+      userId: "Suresh",
+      createdDttm: "2020-08-25T18:32:39.237+00:00",
+      modifiedDttm: "2020-08-25T18:32:39.237+00:00",
+      lookupDetails: [
         {
-          "lookupValue": { "AdjReasonCode": "1" },
-          "id": 4,
-          "keyValue": "CC",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "1" },
+          id: 4,
+          keyValue: "CC",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "2" },
-          "id": 7,
-          "keyValue": "LW",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "2" },
+          id: 7,
+          keyValue: "LW",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "15" },
-          "id": 10,
-          "keyValue": "AA",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "15" },
+          id: 10,
+          keyValue: "AA",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "3" },
-          "id": 123,
-          "keyValue": "EC",
-          "userId": "Surya",
-          "createdDttm": "2020-11-09T15:59:14.583+00:00",
-          "modifiedDttm": "2020-11-09T15:59:14.583+00:00"
+          lookupValue: { AdjReasonCode: "3" },
+          id: 123,
+          keyValue: "EC",
+          userId: "Surya",
+          createdDttm: "2020-11-09T15:59:14.583+00:00",
+          modifiedDttm: "2020-11-09T15:59:14.583+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "31" },
-          "id": 3,
-          "keyValue": "RT",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "31" },
+          id: 3,
+          keyValue: "RT",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "15" },
-          "id": 1,
-          "keyValue": "IC",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "15" },
+          id: 1,
+          keyValue: "IC",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "19" },
-          "id": 5,
-          "keyValue": "RC",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "19" },
+          id: 5,
+          keyValue: "RC",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "1" },
-          "id": 6,
-          "keyValue": "LC",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "1" },
+          id: 6,
+          keyValue: "LC",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "16" },
-          "id": 9,
-          "keyValue": "DS",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "16" },
+          id: 9,
+          keyValue: "DS",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "2" },
-          "id": 2,
-          "keyValue": "VS",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
+          lookupValue: { AdjReasonCode: "2" },
+          id: 2,
+          keyValue: "VS",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
         },
         {
-          "lookupValue": { "AdjReasonCode": "9" },
-          "id": 8,
-          "keyValue": "QI",
-          "userId": "Suresh",
-          "createdDttm": "2020-08-25T18:32:39.237+00:00",
-          "modifiedDttm": "2020-08-25T18:32:39.237+00:00"
-        }
-      ]
-    }
+          lookupValue: { AdjReasonCode: "9" },
+          id: 8,
+          keyValue: "QI",
+          userId: "Suresh",
+          createdDttm: "2020-08-25T18:32:39.237+00:00",
+          modifiedDttm: "2020-08-25T18:32:39.237+00:00",
+        },
+      ],
+    },
   ];
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -130,9 +130,46 @@ function TableView() {
     return (
       <div className="App">
         <Container>
-            <div>
-                dfd
+          <Container>
+            <Row>
+              <Col md={{ span: 2, offset: 10 }}>
+                <div className="float-right">
+                  <a href="#">Add new lookup</a>
+                </div>
+              </Col>
+            </Row>
+            <div className="filter-section">
+              <Row>
+                <Col>
+                  <ul className="filter-list">
+                    <li>
+                      <strong>Region: </strong> USA
+                    </li>
+                    <li>
+                      <strong>Facility:</strong> -
+                    </li>
+                  </ul>
+                </Col>
+                <Col>
+                  <ul className="filter-list">
+                    <li>
+                      <strong>Lookup Name: </strong> Reason code
+                    </li>
+                    <li>
+                      <strong>Lookup Description:</strong> Reason code
+                    </li>
+                  </ul>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={{ span: 1, offset: 11 }}>
+                  <div className="">
+                    <a href="#">Edit</a>
+                  </div>
+                </Col>
+              </Row>
             </div>
+          </Container>
         </Container>
         <Table striped bordered hover>
           <thead>
@@ -150,8 +187,12 @@ function TableView() {
                 <td>{item.id}</td>
                 <td>{item.keyValue}</td>
                 <td>{item.userId}</td>
-                <td>{Moment(item.createdDttm).format('YYYY-MM-DD h:mm:ss A')}</td>
-                <td>{Moment(item.modifiedDttm).format('YYYY-MM-DD h:mm:ss A')}</td>
+                <td>
+                  {Moment(item.createdDttm).format("YYYY-MM-DD h:mm:ss A")}
+                </td>
+                <td>
+                  {Moment(item.modifiedDttm).format("YYYY-MM-DD h:mm:ss A")}
+                </td>
               </tr>
             ))}
           </tbody>
